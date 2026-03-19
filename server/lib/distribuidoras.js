a0f4a473c9c138052cff177934bee4aebd76cb18
Pedro Augusto <work.pedroaugusto@protonmail.com>
2026-03-18 22:54:14 -0300

    feat: aplicar logica de extraçao csv que nao consome tanta memoria ram

--- server/lib/distribuidoras.js
+++ server/lib/distribuidoras.js
@@ -1,15 +1,15 @@
 import { writeFile, readFile } from "node:fs/promises";
-import { obterLinkArquivoANEEL, obterDadosCSV } from "./utils.js";
+import { obterLinkArquivoANEEL, iterarDadosCSV } from "./utils.js";
 
-async function salvarDadosDists(distsResidenciais) {
+async function salvarDados(tarifasPorDist) {
   const conteudoJSON = JSON.stringify({
     dataDaBusca: new Date(),
-    distsResidenciais,
+    tarifasPorDist,
   });
-  return writeFile("server/data/dados-dists-residenciais.json", conteudoJSON);
+  return writeFile("server/data/tarifas.json", conteudoJSON);
 }
 
-async function lerDadosDistsAnteriores() {
+async function lerDadosAnteriores() {
   try {
     const conteudoJSON = await readFile("server/data/dados-dists-residenciais.json");
     return JSON.parse(conteudoJSON);
@@ -18,63 +18,77 @@ async function lerDadosDistsAnteriores() {
       return {
         // Caso o arquivo backup não exista, retorne um resultado no formato
         // apropriado, porém com uma data muito antiga.
-        // Desta forma, o programa irá buscar novos dados de distribuidoras da ANEEL.
+        // Desta forma, o programa irá buscar novos dados de tarifas da ANEEL.
         dataDaBusca: new Date(0),
-        distsResidenciais:  [],
+        tarifasPorDist:  [],
       };
     }
   }
 }
 
-async function obterDistsResidenciais() {
-  let { dataDaBusca, distsResidenciais } = await lerDadosDistsAnteriores();
+async function obterTarifasPorDist() {
+  let { dataDaBusca, tarifasPorDist } = await lerDadosAnteriores();
   // Se uma busca bem sucedida ocorreu nas últimas 24 horas, use ela ao
   // invés da baixar os dados novamente.
   const UM_DIA_EM_MS = 1000 * 60 * 60 * 24;
   const diferençaTempo = new Date() - new Date(dataDaBusca).getTime();
   if( diferençaTempo <= UM_DIA_EM_MS) {
-    return distsResidenciais;
+    return tarifasPorDist;
   }
+
   // Faça uma nova busca na API da ANEEL caso já faça mais de 24 horas
   // desde a última.
+  tarifasPorDist = {};
   const url = await obterLinkArquivoANEEL(
     "tarifas-distribuidoras-energia-eletrica",
     "CSV"
   );
-  const todasDistribuidoras = await obterDadosCSV(url);
-  const anoAtual = String(new Date().getFullYear());
-  const siglasDistribuidoras = [];
-  distsResidenciais = todasDistribuidoras
-    .filter(d => (
-      // Filtrar por tarifas residenciais do ano atual.
-      d.DscClasse === "Residencial"
-      && d.DscBaseTarifaria.includes("Tarifa")
-      && d.VlrTE
-      && d.SigAgente
-      && d.DatInicioVigencia.includes(anoAtual)
-      && d.DscUnidadeTerciaria === "MWh"
-    ))
-    .sort((distA, distB) => {
-      // Reordenar o resultado com base no início de vigência da tarifa.
-      // Tarifas mais recentes primeiro.
-      const vigenciaA = new Date(distA.DatInicioVigencia);
-      const vigenciaB = new Date(distB.DatInicioVigencia);
-      return vigenciaB - vigenciaA;
-    })
-    .flatMap(d => {
-      // Apenas salve a tarifa mais recente, descartando as antigas.
-      if (siglasDistribuidoras.includes(d.SigAgente)) {
-        return [];
+  for await (const dist of iterarDadosCSV(url)) {
+    // Se o objeto de distribuidora atual não passar nestes critérios,
+    // ignore e vá para o próximo.
+    if (!(
+      dist.DscClasse === "Residencial"
+      && dist.DscBaseTarifaria.includes("Tarifa")
+      && dist.VlrTE
+      && dist.SigAgente
+      && dist.DscUnidadeTerciaria === "MWh"
+    )) {
+      continue;
+    }
+    const nomeDist = dist.SigAgente;
+    const tarifaAtual = {
+        tarifaEnergiaKwh: parseFloat(dist.VlrTE.replace(',', '.')) / 1000,
+        tarifaUsoKwh: dist.VlrTUSD ? parseFloat(dist.VlrTUSD.replace(',', '.')) / 1000 : null,
+        inicioVigencia: dist.DatInicioVigencia,
+    };
+    // Se a tarifa atual que esta sendo examinar neste loop for de uma distribuidora
+    // já avaliada anteriormente, cheque primeiro se a data de início de vigência for
+    // mais recente do que a última.
+    // Se for o caso, substitua a que foi salva anteriormente pela atual, pois o objetivo
+    // é ficar apenas com as tarifas mais recentes de cada distribuidora.
+    if (nomeDist in tarifasPorDist) {
+      const vigenciaAtual = new Date(tarifaAtual.inicioVigencia);
+      const vigenciaSalva = new Date(tarifasPorDist[nomeDist].inicioVigencia);
+      const diferençaTempo = vigenciaAtual - vigenciaSalva;
+      if (diferençaTempo > 0) {
+        tarifasPorDist[nomeDist] = tarifaAtual;
+      } else {
+        continue;
       }
-      siglasDistribuidoras.push(d.SigAgente);
-      return [{
-        nome: d.SigAgente,
-        tarifaEnergiaKwh: parseFloat(d.VlrTE.replace(',', '.')) / 1000,
-        tarifaUsoKwh: d.VlrTUSD ? parseFloat(d.VlrTUSD.replace(',', '.')) / 1000 : null,
-        inicioVigencia: d.DatInicioVigencia,
-      }];
-    });
-  
-  await salvarDadosDists(distsResidenciais);
-  return distsResidenciais;
+    } 
+    tarifasPorDist[nomeDist] = tarifaAtual;
+  }
+  await salvarDados(tarifasPorDist);
+  return tarifasPorDist;
+}
+
+async function obterDistPorMunicipio() {
+  const url = await obterLinkArquivoANEEL(
+    "base-de-dados-geografica-da-distribuidora-bdgd",
+    "ZIP",
+  );
+
+  for await (const linha of iterarDadosCSV(url)) {
+    console.log(linha);
+  }
 }
--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,5 +1,6 @@
-import { parse } from "csv-parse"
-import { Readable } from "node:stream"
+import { parse } from "csv-parse";
+import { Readable } from "node:stream";
+import unzipper from "unzipper";
 
 // Conjutos de dados no site da ANEEL possuem arquivos de diferentes formatos
 // associados a eles. Essa função permite que buscar o link de um arquivo de
@@ -31,17 +32,33 @@ export async function obterLinkArquivoANEEL(conjuntoDados, formato) {
   return arquivo.url;
 }
 
-export async function obterDadosCSV(url) {
+export async function* iterarDadosCSV(url) {
   const resposta = await fetch(url);
 
-  return new Promise((resolve, reject) => {
-    const resultados = [];
-    // Converta cada fileira num objeto cujo as propriedades são o cabeçalho
-    // de cada coluna, e adiciona na lista de resultados.
-    Readable.fromWeb(resposta.body)
-      .pipe(parse({ columns: true, delimiter: ";" }))
-      .on("data", (fileira) => resultados.push(fileira))
-      .on("end", () => resolve(resultados))
-      .on("error", (err) => reject(err));
-  });
+  if (!resposta.ok) {
+    throw new Error(`Erro ao baixar: ${resposta.status}`);
+  }
+
+  // Checa se aquilo que foi baixado é um arquivo ZIP.
+  const contentType = resposta.headers.get("content-type") || "";
+  const temZip =
+    url.endsWith(".zip") ||
+    contentType.includes("zip");
+
+  let stream = Readable.fromWeb(resposta.body);
+  if (temZip) {
+    stream = stream
+      .pipe(unzipper.ParseOne()) // pega o primeiro arquivo dentro do zip
+  }
+  // Converta cada fileira num objeto cujo as propriedades são o cabeçalho
+  // de cada coluna, e adiciona na lista de resultados.
+  const parseador = stream.pipe(
+    parse({
+      columns: true,
+      delimiter: ";",
+    })
+  );
+  for await (const linha of parseador) {
+    yield linha;
+  }
 }
