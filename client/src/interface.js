eac2c4f724c3c5b1a69565103b600e641bc3286a
Pedro Augusto <work.pedroaugusto@protonmail.com>
2026-03-31 21:47:31 -0300

    fix: consertar seletor de municipio que iniciava sem valor

--- client/src/interface.js
+++ client/src/interface.js
@@ -1,10 +1,59 @@
-export function preencherSelectEstado(select, estados) {
+import { obterLocalidades } from "./obter-dados.js";
+
+// Conjunto de dados das APIs.
+const { estados, municipios } = await obterLocalidades();
+
+export function inicializarSeletorEstado() {
+  const label = document.createElement("label");
+  label.htmlFor = "estado";
+  label.textContent = "Estado";
+  const seletor = document.createElement("select");
+  seletor.name = "estado";
+  seletor.id = "estado";
+  seletor.required = true;
+
   for (const { sigla, nome } of estados) {
     const option = document.createElement("option");
     option.value = sigla;
     option.textContent = nome;
-    select.append(option);
+    seletor.append(option);
   }
+  const fieldset = document.getElementById("localidade");
+  fieldset.append(label, seletor);
+}
+
+export function inicializarSeletorMunicipio() {
+  // Criar seletor e respectivo "label".
+  const label = document.createElement("label");
+  label.htmlFor = "municipio";
+  label.textContent = "Município";
+  const seletor = document.createElement("select");
+  seletor.name = "municipio";
+  seletor.id = "municipio";
+  seletor.required = true;
+
+  const seletorEstado = document.getElementById("estado");
+
+  function atualizarSeletorMunicipio() {
+    // Remova os municípios do Estado anterior.
+    while (seletor.hasChildNodes()) {
+      seletor.firstChild.remove();
+    }
+    const siglaEstadual = seletorEstado.value;
+    for (const municipio of municipios[siglaEstadual]) {
+      const option = document.createElement("option");
+      option.value = municipio.id;
+      option.textContent = municipio.nome;
+      seletor.append(option);
+    }
+  }
+  // Use a função uma vez para inicializar a opções, e a chame novamente
+  // sempre que o Estado for alterado.
+  atualizarSeletorMunicipio();
+  seletorEstado.addEventListener("change", atualizarSeletorMunicipio);
+
+  const fieldset = document.getElementById("localidade");
+  fieldset.append(label, seletor);
 }
 
 export function criarTabela(resultados) {
--- client/src/main.js
+++ client/src/main.js
@@ -1,31 +1,16 @@
-import { obterLocalidades } from "./obter-dados.js";
 import { calcularResultados} from "./utilidades.js";
-import { criarTabela, preencherSelectEstado } from "./interface.js";
+import { criarTabela, inicializarSeletorEstado, inicializarSeletorMunicipio } from "./interface.js";
 
-// Conjunto de dados das APIs.
-const { estados, municipios } = await obterLocalidades();
 
 // Buscar elementos da página.
-const selectEstado = document.getElementById("estado");
-const selectMunicipio = document.getElementById("municipio");
+const seletorEstado = document.getElementById("estado");
+const seletorMunicipio = document.getElementById("municipio");
 const inputValorConta = document.getElementById("valor-conta");
 const botaoCalcular = document.getElementById("botao-calcular");
 
 // Seletores de Estado e Município.
-preencherSelectEstado(selectEstado, estados);
-selectEstado.addEventListener("change", (event) => {
-  // Remova os municípios do Estado anterior.
-  while (selectMunicipio.hasChildNodes()) {
-    selectMunicipio.firstChild.remove();
-  }
-  const siglaEstadual = selectEstado.value;
-  for (const municipio of municipios[siglaEstadual]) {
-    const option = document.createElement("option");
-    option.value = municipio.id;
-    option.textContent = municipio.nome;
-    selectMunicipio.append(option);
-  }
-});
+inicializarSeletorEstado();
+inicializarSeletorMunicipio();
 
 botaoCalcular.addEventListener("click", function(event) {
   event.preventDefault();
